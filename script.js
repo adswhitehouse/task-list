2388dbce689a01b91db114f946fce366fe4815e9
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-14 16:49:32 +0000

    made orange check button interactive

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
 const input = document.querySelector(".jsTaskInput");
 const addBtn = document.querySelector(".jsAddTaskBtn");
-const checkBtn = document.querySelector(".jsTaskCheck");
 
 const taskListContainer = document.querySelector(".jsTaskListContainer");
-const taskItem = document.querySelector(".jsTaskListItem");
 
 function createTask() {
-  let taskText = input.value
-  let newTaskItem = document.createElement("div");
-  newTaskItem.classList.add("task-list-item");
-  newTaskItem.innerHTML = `
-    <p class="task-text">${taskText}</p>
-    <div class="buttons">
-      <button class="task-check jsTaskCheck"><i class="fa-solid fa-check"></i></button>
-      <button class="task-delete jsTaskDelete"><i class="fa-solid fa-trash"></i></button>
-    </div>`;
-    taskListContainer.appendChild(newTaskItem)
-    input.value = ``
+  let taskText = input.value;
+  if (taskText) {
+    let newTaskItem = document.createElement("div");
+    newTaskItem.classList.add("task-list-item");
+    newTaskItem.innerHTML = `
+      <p class="task-text">${taskText}</p>
+      <div class="buttons">
+        <button class="task-check jsTaskCheck"><i class="fa-solid fa-check"></i></button>
+        <button class="task-delete jsTaskDelete"><i class="fa-solid fa-trash"></i></button>
+      </div>`;
+    taskListContainer.appendChild(newTaskItem);
+    input.value = ``;
+  }
 
-    let deleteBtn = document.querySelectorAll(".jsTaskDelete");
-    deleteBtn.forEach((button) => {
-      button.addEventListener("click", () => {
-        button.parentElement.parentElement.remove()
-      })
-    })
+  let deleteBtn = document.querySelectorAll(".jsTaskDelete");
+  deleteBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.parentElement.parentElement.remove();
+    });
+  });
+
+  const checkBtn = document.querySelectorAll(".jsTaskCheck");
+  checkBtn.forEach((button) => {
+    let isLineThrough = false;
+    button.addEventListener("click", () => {
+      if (!isLineThrough) {
+        isLineThrough = true;
+        button.parentElement.parentElement.firstElementChild.classList.add(
+          "line-through"
+        );
+        button.classList.add("task-checked");
+      } else {
+        isLineThrough = false;
+        button.parentElement.parentElement.firstElementChild.classList.remove(
+          "line-through"
+        );
+        button.classList.remove("task-checked");
+      }
+    });
+  });
 }
 
 addBtn.addEventListener("click", () => {
-  createTask()
-})
+  createTask();
+});
 
 input.addEventListener("keydown", (e) => {
-  if(e.key === "Enter") {
-    createTask()
+  if (e.key === "Enter") {
+    createTask();
   }
-})
+});
